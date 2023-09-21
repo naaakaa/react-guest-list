7ce4a12f9b3e4eab2f1b125222ec49d864f8f71d
Anastasia <ana.kuk@gmx.net>
2023-09-21 12:03:10 +0200

    Exchange svg file, delete unneeded files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
+import logo from './guest-add-svgrepo-com.svg';
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>React Guest List</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
