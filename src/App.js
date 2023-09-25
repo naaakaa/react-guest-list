5f42171895cf2f2c9cc828838c4de03b913da558
Anastasia <ana.kuk@gmx.net>
2023-09-25 09:52:07 +0200

    Add basic design, remove unused files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,67 @@
-import logo from './guest-add-svgrepo-com.svg';
+import { useEffect, useState } from 'react';
+
+const baseUrl = 'http://localhost:4000';
+
+let response = await fetch(`${baseUrl}/guests`);
+const allGuests = await response.json();
+
+let guestList = [
+  { firstName: 'First', lastName: 'Meier', attending: true },
+  { firstName: 'Second', lastName: 'Berger', attending: false },
+];
 
 export default function App() {
+  const [firstName, setFirstName] = useState('');
+  function handleChange(event) {
+    setFirstName(event.target.value);
+  }
+  const [lastName, setLastName] = useState('');
+  function handleOtherChange(event) {
+    setLastName(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>React Guest List</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Guests:
+      {guestList.map((guest) => {
+        return (
+          <div key={`guest-div-${guest.firstName}`}>
+            <br />
+            Name: {guest.firstName} {guest.lastName}
+            <br />
+            Attending: {guest.attending}
+            <br />
+          </div>
+        );
+      })}
+      <form>
+        <br />
+        <p>Please enter the guest's name here:</p>
+        <label>
+          First name
+          <br />
+          <input onChange={handleChange} value={firstName} />
+        </label>
+        <br />
+        <br />
+        <label>
+          Last name
+          <br />
+          <input onChange={handleOtherChange} value={lastName} />
+        </label>
+        <br />
+        <br />
+        <label aria-label="<first name> <last name> attending status">
+          Attending
+          <input type="checkbox" />
+        </label>{' '}
+        <br />
+        <br />
+        <button>Return</button>
+        <br />
+        <br />
+        <button>Remove</button>
+      </form>
     </div>
   );
 }
---
+++ src/App2.js
@@ -0,0 +1,80 @@
+import { useEffect, useState } from 'react';
+
+const baseUrl = 'http://localhost:4000';
+
+let id = 0;
+
+export default function NamesList() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [attending, setAttending] = useState('');
+  const [users, setUsers] = useState([]);
+
+  function handleChange(event) {
+    setFirstName(event.target.value);
+  }
+
+  function handleOtherChange(event) {
+    setLastName(event.target.value);
+  }
+
+  function handleThirdChange(event) {
+    setAttending(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    const newUsers = [
+      {
+        id: id,
+        firstName: firstName,
+        lastName: lastName,
+        attending: attending,
+      },
+      ...users,
+    ];
+    id++;
+    setUsers(newUsers);
+    setFirstName('');
+    setLastName('');
+    setAttending('');
+    event.preventDefault();
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        onChange={handleChange}
+        value={firstName}
+        placeholder="First name"
+      />
+      <input
+        onChange={handleOtherChange}
+        value={lastName}
+        placeholder="Last name"
+      />
+      <label
+        onChange={handleThirdChange}
+        value={attending}
+        aria-label="<first name> <last name> attending status"
+      >
+        Attending
+        <input type="checkbox" />
+      </label>{' '}
+      <br />
+      <br />
+      <button>Return</button>
+      <br />
+      <br />
+      Guests:
+      {users.map((user) => {
+        return (
+          <div key={`user-${user.id}`}>
+            {user.firstName} {user.lastName} (on if attending) {user.attending}
+          </div>
+        );
+      })}
+      <br />
+      <br />
+    </form>
+  );
+}
